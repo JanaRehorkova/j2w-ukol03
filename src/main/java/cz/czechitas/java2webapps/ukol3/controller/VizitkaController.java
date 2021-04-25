4b8918191d0f1da1f1154855202f512556df2f69
JanaRehorkova <Morav.Janina@gmail.com>
2021-04-25 19:07:59 +0200

    Ahoj Vítku, toto je moje řešení domácího úkolu č.3, nešly mi zobrazit mapy (ani z původního kódu od Filipa), třeba to pojede u Tebe :)
    Janča

--- src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol3/controller/VizitkaController.java
@@ -1,20 +1,44 @@
 package cz.czechitas.java2webapps.ukol3.controller;
 
+import cz.czechitas.java2webapps.ukol3.entity.Vizitka;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Kontroler obsluhující zobrazování vizitek.
  */
 @Controller
 public class VizitkaController {
+  private final List<Vizitka> vizitky;
+
+  public VizitkaController(){
+    vizitky=List.of(
+            new Vizitka("John Lawrence","Eagle Fang Karate","Jurkovičova 2","63800 Brno","strikefirst@eaglefang.com",null,null),
+            new Vizitka("Daniel LaRusso","Miagi-do Karate","Zeleného 10","60200 Brno","DannyLaRusso@miagi-do.com","+420 605528124","www.miagi-do.com"),
+            new Vizitka("John Kreese","Cobra Kai Karate","Stará Osada 3984/15","61500 Brno","nomercy@cobrakai.com","+420 777658243",null),
+            new Vizitka("Miguel Diaz","Eagle Fang Karate","Jurkovičova 5","63800 Brno","MiguelDiaz@eaglefang.com","+420 608748264","www.eaglefang.com")
+    );
+
+  }
+
+
+
   @GetMapping("/")
-  public String seznam() {
-    return "seznam";
+public ModelAndView vizitka(){
+    ModelAndView modelAndView = new ModelAndView("seznam");
+    modelAndView.addObject("seznamvizitek",vizitky);
+    return modelAndView;
   }
 
   @GetMapping("/detail")
-  public String detail() {
-    return "detail";
+  public ModelAndView detail(int id){
+    ModelAndView modelAndView = new ModelAndView("detail");
+    modelAndView.addObject("detaily",vizitky.get(id));
+    return modelAndView;
   }
+
+
 }
